60aca906c54df4c46089480d86cd1f193781de3a
yong-gyun <sygttgudthfr@gmail.com>
2023-05-17 18:14:33 +0900

    no message

--- Algorithm/Excercise/Dijkstra.cs
+++ Algorithm/Excercise/Dijkstra.cs
@@ -20,11 +20,78 @@ namespace Excercise
             { -1, -1, -1, -1, 5, -1}
         };
 
+        public void Excercise_Dijkstra(int start = 0)
+        {
+
+        }
+
         public void Run_Dijkstra(int start)
         {
             bool[] visited = new bool[6];       //이젠 찾았다가 아닌 방문했다가 중요함
             int[] distance = new int[6];        //정점에서 정점으로 이동하는데에 필요한 가중치를 기입
+            int[] parent = new int[6];
             Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+
+            while(true)
+            {
+                //제일 좋은 후보를 찾는다 (가장 가까이에 있는 후보)
+
+                //가장 유력한 후보의 거리와 번호를 저장한다.
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    //이미 방문한 정점은 스킵
+
+                    if (visited[i])
+                        continue;
+
+                    //아직 발견(예약)된 적이 없거나 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+
+                    //여태껏 발견한 가장 좋은 후보라는 의미니까 정보를 갱신
+                    closest = distance[i];
+                    now = i;
+                }
+
+                //다음 후보가 하나도 없다(모든 정점을 찾았거나 연결이 단절되어 있는 경우) -> 종료
+                if (now == -1)
+                    break;
+
+                //제일 좋은 후보를 찾았으니까 방문한다
+                visited[now] = true;
+
+                //방문한 정점과 인접한 정점들을 조사해서,
+                //상황에 따라 발견한 최단 거리를 갱신한다.
+
+                for (int next = 0; next < 6; next++)
+                {
+                    //연결되지 않은 정점은 스킵
+                    if (adj[now, next] == -1)
+                        continue;
+
+                    //이미 방문한 정점은 스킵
+                    //ex) 1번에서 0번을 조회해보니 연결은 되어있긴한데 이미 방문했던적이 있으니 뒤로 돌아갈 필요가 없음.
+                    if (visited[next])
+                        continue;
+
+                    //새로 조사된 정점의 최단거리를 계산한다.
+                    int nextDist = distance[now] + adj[now, next];
+
+                    //만약에 기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면, 정보를 갱신
+
+                    if(nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
         }
     }
 }
--- Algorithm/Excercise/Program.cs
+++ Algorithm/Excercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
@@ -8,74 +9,57 @@ using System.Threading.Tasks;
 
 namespace Excercise
 {
-    class Graph
+    class TreeNode<T>
     {
-        int[,] adj = new int[6, 6]
-        {
-            { 0, 1, 0, 1, 0, 0},
-            { 1, 0, 1, 1, 0, 0},
-            { 0, 1, 0, 0, 0, 0},
-            { 1, 1, 0, 0, 1, 0},
-            { 0, 0, 0, 1, 0, 1},
-            { 0, 0, 0, 0, 1, 0}
-        };
-
-        List<int>[] adj2 = new List<int>[]
-        {
-            new List<int>() { 1, 3 },
-            new List<int>() { 0, 2, 3},
-            new List<int>() { 1 },
-            new List<int>() { 0, 1, 4 },
-            new List<int>() { 3, 5},
-            new List<int>() { 4 },
-        };
-
-        public void BFS(int start)
-        {
-            bool[] found = new bool[6];
-            
-            int[] parent = new int[6];    //내가 어디서부터 왔는지 추척하기 위한 배열
-            int[] distance = new int[6];  //오기까지 얼마나 걸렸는지를 확인하기 위한 배열
-
-            Queue<int> q = new Queue<int>();
-            
-            q.Enqueue(start);
-            found[start] = true;
-            parent[start] = start;
-            distance[start] = 0;
-
-            while(q.Count > 0)
-            {
-                int now = q.Dequeue();
-                Console.WriteLine(now);
-
-                for (int next = 0; next < 6; next++)
-                {
-                    //인접하지 않았으면 스킵
-                    if (adj[now, next] == 0)
-                        continue;
-
-                    //이미 방문한 애라면 스킵
-                    if (found[next] == true)
-                        continue;
-
-                    q.Enqueue(next);
-                    found[next] = true;
-                    //1번의 부모는 0번이됌
-                    parent[next] = now;
-                    //이전까지 이동한 거리 + 1
-                    distance[next] = distance[now] + 1;
-                }
-            }
-        }
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Graph graph = new Graph();
-            graph.BFS(0);
+            TreeNode<string> root = MakeTree();
+        }
+
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "디자인팀" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+            }
+
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            //접근
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Children)
+            {
+                PrintTree(root);
+            }
         }
     }
 }
